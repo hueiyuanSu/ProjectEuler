df38fb0706c270c20b6c5b04d4b7e1ebfaa9afc0
hueiyuan_su <max19950618@gmail.com>
2017-11-28 18:51:18 +0800

    problem 02

--- Problem02.java
+++ Problem02.java
@@ -1,7 +1,23 @@
 import java.util.*;
 
 class Problem02{
-    public static void main(String[] args){
+    public static int Fibonacci(){
+        int item = 0;
+        int item2 = 1;
+        int temp = 0, sum = 0;
+        while(item <= 4000000){
+            temp = item + item2;
+            item = item2;
+            item2 = temp;
+            if(temp % 2 == 0){
+                sum += temp;
+            }
+        }
 
+        return sum;
+    }
+
+    public static void main(String[] args){
+        System.out.println(Fibonacci());
     }
 }
